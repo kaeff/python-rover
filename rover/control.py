75c8184aa6a6e79ca351923c986dabf6fc9dfbb5
kaeff <hi@kaeff.net>
2015-10-15 17:54:20 +0200

    Draw the rest of the fucking owl

--- rover/control.py
+++ rover/control.py
@@ -1,3 +1,47 @@
-def launch_mission(input):
-    return """1 3 N
-5 1 E"""
+HEADINGS = ['N', 'E', 'S', 'W']
+LEFT_RIGHT = {'L': -1, 'R': 1}
+
+
+def launch_mission(input_string):
+    rover_lines = input_string.splitlines()[1:]
+    rovers = pairs(rover_lines)
+    final_positions = [execute_mission(rover) for rover in rovers]
+    return '\n'.join(["%s %s %s" % final_position for final_position in final_positions])
+
+
+def pairs(a_list):
+    result = []
+    while a_list:
+        result.insert(0, (a_list.pop(), a_list.pop()))
+    return result
+
+
+def execute_mission(rover):
+    commands, initial_position = rover
+    x, y, heading = initial_position.split(' ')
+    for command in commands:
+        x, y, heading = execute_command(x, y, heading, command)
+    return x, y, heading
+
+
+def execute_command(x, y, heading, command):
+    x, y = int(x), int(y)
+
+    if command is 'M':
+        x, y = execute_move(heading, x, y)
+    else:
+        heading = execute_turn(heading, command)
+
+    return x, y, heading
+
+
+def execute_move(heading, x, y):
+    y = y + 1 if heading is 'N' else y
+    y = y - 1 if heading is 'S' else y
+    x = x + 1 if heading is 'E' else x
+    x = x - 1 if heading is 'W' else x
+    return x, y
+
+
+def execute_turn(heading, command):
+    return HEADINGS[(HEADINGS.index(heading) + LEFT_RIGHT[command]) % 4]
--- test/control_test.py
+++ test/control_test.py
@@ -17,3 +17,29 @@ MMRMMRMRRM"""
         actual = control.launch_mission(input)
 
         self.assertEqual(actual, expected)
+
+    def test_simple_move(self):
+        commands_from_0_0_to_0_1 = """0 1\n0 0 N\nM"""
+        expected = "0 1 N"
+
+        actual = control.launch_mission(commands_from_0_0_to_0_1)
+
+        self.assertEqual(actual, expected)
+
+    def test_move(self):
+        self.assertEqual((2, 3), control.execute_move('N', 2, 2))
+        self.assertEqual((3, 2), control.execute_move('E', 2, 2))
+        self.assertEqual((2, 1), control.execute_move('S', 2, 2))
+        self.assertEqual((1, 2), control.execute_move('W', 2, 2))
+
+    def test_turn_right(self):
+        self.assertEqual('E', control.execute_turn('N', 'R'))
+        self.assertEqual('S', control.execute_turn('E', 'R'))
+        self.assertEqual('W', control.execute_turn('S', 'R'))
+        self.assertEqual('N', control.execute_turn('W', 'R'))
+
+    def test_turn_left(self):
+        self.assertEqual('W', control.execute_turn('N', 'L'))
+        self.assertEqual('S', control.execute_turn('W', 'L'))
+        self.assertEqual('E', control.execute_turn('S', 'L'))
+        self.assertEqual('N', control.execute_turn('E', 'L'))
